fd6a84acc9f3bb4453cf5353ad0fec3c1ae1e6d2
unknown <zkk12345@gmail.com>
2013-06-10 23:48:28 +0300

    Project: move functions: 	'SaveResult' and 'CreateTestResultFolder' 	into their own files.

---
+++ Project/Tests/Misc Functions/SaveResults.m
@@ -0,0 +1,16 @@
+function [ ] = SaveResults(OriginalPic_RGB , FixedPic_RGB , FigureName, IsYIQ)   
+    figure('name',FigureName,'NumberTitle','off');
+    subplot(2,2,1), imshow(OriginalPic_RGB), title('Original pic');
+    subplot(2,2,2), imshow(protanopes(OriginalPic_RGB)), title('Original pic through protanopes eyes');
+    if(IsYIQ == 1)
+        subplot(2,2,3), imshow(FixedPic_RGB/256), title('Fixed pic'); 
+    else
+        subplot(2,2,3), imshow(FixedPic_RGB), title('Fixed pic'); 
+    end
+    subplot(2,2,4), imshow(protanopes(FixedPic_RGB)), title('Fixed pic through protanopes eyes ');  
+
+    FileName = strcat('Test results/',FigureName);
+    saveas(gcf,FileName, 'jpg');
+    %close gcf;  
+end
+
